d76a255a619d9f9d4d79fb671f256961170890f7
Alexander Orchard <alex.orchard@intelliware.com>
2016-05-10 17:20:23 -0400

    Basic models

--- SwiftYelp/Business.swift
+++ SwiftYelp/Business.swift
@@ -8,6 +8,42 @@
 
 import UIKit
 
-class Business: NSObject {
+class Business {
 
+    var serverId: String?
+    var name: String?
+    var rating: String?
+    var reviewCount: String?
+    var snippetText: String?
+    
+    var claimed: String?
+    var closed: String?
+    
+    var mobileUrl: String?
+    var imageUrl: String?
+    var ratingImageUrl: String?
+    var ratingImageLargeUrl: String?
+    var snippetImageUrl: String?
+    
+    var categories: Array<String>?
+    var location: Location?
+    
+    class func fromArray(array: Array<Dictionary<String, AnyObject>>) -> Array<Business> {
+        var result = Array<Business>()
+        
+        for json in array {
+            result.append(Business.fromJson(json))
+        }
+        
+        return result;
+    }
+    
+    class func fromJson(json: Dictionary<String, AnyObject>) -> Business {
+        let result = Business()
+        
+        result.serverId = json["server_id"] as? String
+        
+        return result;
+    }
+    
 }
---
+++ SwiftYelp/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  SwiftYelp
+//
+//  Created by Alexander Orchard on 2016-05-10.
+//  Copyright © 2016 Intelliware Development. All rights reserved.
+//
+
+import UIKit
+
+class Location  {
+
+}
